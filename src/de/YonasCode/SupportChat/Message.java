862aa62e007a61c42aa78e6cc198a8ab6a9a82ca
MoD2Cody <heimann2000@hotmail.de>
2014-02-20 20:07:02 +0100

    Update Message.java [Deutsch]
    Hey, Yonas!
    
    Ich hab kurzerhand alles schnell übersetzt, da mir langweilig war. Es klingt vielleicht ein wenig komisch, dafür habe ich alle umlaute umgangen, das bringt nur Ärger. Außerdem wäre es sehr nett, wenn man das selbst konfigurieren könnte. Wäre sicher nicht so schwer, oder? Außerdem kannst du anstatt ChatColor.Red, etc. einfach §(Farbcode) nehmen. §5=&5 ingame.
    Finde deine Plugins super! Habe alle.
    
    Liebe Grüße
    Cody

--- src/de/YonasCode/SupportChat/Message.java
+++ src/de/YonasCode/SupportChat/Message.java
@@ -4,15 +4,15 @@ import org.bukkit.ChatColor;
 
 public class Message {
 	
-	public static final String TAG 				= ChatColor.GOLD + "[" + ChatColor.AQUA + "SupportChat" + ChatColor.GOLD + "] ";
-	public static final String NO_PERMISSIONS 	= ChatColor.RED + "Sorry, but you're not permitted to use this command.";
-	public static final String MORE_WORDS			= ChatColor.RED + "Please enter more words.";
-	public static final String IN_WAITLIST		= ChatColor.RED + "You're already in the waitlist.";
-	public static final String ADDED_WAITLIST 	= ChatColor.GREEN + "You're now in the waitlist.";
-	public static final String ALREADY_IN_CHAT 	= ChatColor.RED + "You're already in a support chat.";
-	public static final String EMPTY_WAITLIST	= ChatColor.RED + "The waitlist is empty.";
-	public static final String LEAVE_CHAT		= ChatColor.GREEN + "The chat was successfully closed.";
-	public static final String NEED_SUPPORTER	= ChatColor.RED + "You must be a supporter.";
-	public static final String NEED_CHAT		= ChatColor.RED + "You must be in a support chat.";
+	public static final String TAG 				= "§6[§4Support§6-§9Chat§6]";
+	public static final String NO_PERMISSIONS 	= ChatColor.RED + "Du besitzt nicht die erforderten Berechtungen, um diesen Befehl zu benutzen!";
+	public static final String MORE_WORDS			= ChatColor.RED + "Erfordert mindestens ein Argument (Wort), um den Support zu informieren. Bei einer Frage kannst du mehrere Argumente angeben.";
+	public static final String IN_WAITLIST		= ChatColor.RED + "Du bist schon in der Warteliste, Gedulde dich, bis ein Supporter auf dich zukommt!";
+	public static final String ADDED_WAITLIST 	= ChatColor.GREEN + "Du hast ein Ticket in der Warteliste erstellt, ein Supporter wird dir bald helfen.";
+	public static final String ALREADY_IN_CHAT 	= ChatColor.RED + "Du bist schon in einem SupportChat, beende diesen, um in einen anderen zu wechseln.";
+	public static final String EMPTY_WAITLIST	= ChatColor.GREEN + "Die Warteliste ist leer. Bisher keine Arbeit :)";
+	public static final String LEAVE_CHAT		= ChatColor.GREEN + "Der SupportChat wurde erfolgreich geschlossen.";
+	public static final String NEED_SUPPORTER	= ChatColor.RED + "Du musst ein Supporter sein, um ein Ticket zu akzeptieren.";
+	public static final String NEED_CHAT		= ChatColor.RED + "Du musst in einem SupportChat sein, um diesen Befehl zu benutzen.";
 	
 }
